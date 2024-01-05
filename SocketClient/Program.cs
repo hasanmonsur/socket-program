7407d425eeee86e6b2c8f709123c02e72381c19c
MD HASAN MONSUR <hasanmcse@gmail.com>
2024-01-05 19:19:42 +0600

    hasan002

---
+++ SocketClient/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json.Serialization;
+
+
+Console.WriteLine("Hello, World! Client");
+
+
+
+try
+{
+    TcpClient client = new TcpClient("127.0.0.1", 8888);
+    Console.WriteLine("Connected to the server...");
+
+    NetworkStream stream = client.GetStream();
+    var requestInfo = new RequestInfo();
+
+    
+    // Send data to the server
+    string message = JsonConvert.SerializeObject(requestInfo); ;// "Hello, server! Hasan Monsur";
+
+    // Create an ISO 8583 message
+
+   
+    byte[] data = Encoding.ASCII.GetBytes(message);
+
+    stream.Write(data, 0, data.Length);
+    Console.WriteLine($"Sent data: {message}");
+
+    // Receive response from the server
+    byte[] responseBuffer = new byte[1024];
+    int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+
+    string responseData = Encoding.ASCII.GetString(responseBuffer, 0, bytesRead);
+    Console.WriteLine($"Received response: {responseData}");
+
+    // Close the connection
+    client.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Exception: {e.Message}");
+}
+
+
+
+public class RequestInfo
+{
+    public RequestInfo()
+    {
+        processCode = "0001";
+        processData =new ProcessData();
+    }
+    public string processCode { get; set; }
+
+    public ProcessData processData { get; set; }
+
+}
+
+public class ProcessData
+{
+    public ProcessData()
+    {
+        id = "20230001";
+        name = "Md Hasan Monsur";
+    }
+    public string id  { get; set; }
+
+    public string name { get; set; }
+
+}
+
---
+++ SocketServer/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+Console.WriteLine("Hello, World! Server");
+
+
+TcpListener server = null;
+
+try
+{
+    // Set the TcpListener on a specific port
+    int port = 8888;
+    IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+    // TcpListener
+    server = new TcpListener(localAddr, port);
+
+    // Start listening for client requests
+    server.Start();
+
+    Console.WriteLine($"Server started on {localAddr}:{port}");
+
+    // Enter the listening loop
+    while (true)
+    {
+        Console.WriteLine("Waiting for a connection...");
+
+        // Perform a blocking call to accept requests
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine($"Connected to {((IPEndPoint)client.Client.RemoteEndPoint).Address}");
+
+        // Handle the client in a separate thread or asynchronously
+        HandleClient(client);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Exception: {e.Message}");
+}
+finally
+{
+    // Stop listening for new clients
+    server?.Stop();
+}
+
+static void HandleClient(TcpClient client)
+{
+    NetworkStream stream = client.GetStream();
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+
+    try
+    {
+        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine($"Received data: {data}");
+
+            //data = data + " Good Morning !!";
+            Console.WriteLine($"Send  data: {data}");
+
+
+            // Echo the data back to the client
+            byte[] responseBuffer = Encoding.ASCII.GetBytes(data);
+            stream.Write(responseBuffer, 0, responseBuffer.Length);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Exception: {e.Message}");
+    }
+    finally
+    {
+        client.Close();
+    }
+}
